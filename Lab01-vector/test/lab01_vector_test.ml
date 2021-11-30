243ea34c9810c99a79e6ff912d18379b5be6d5b7
Vladislav Prekel <misterptits@yandex.ru>
2021-11-30 12:59:06 +0700

    AlcotestExt

--- Lab01-vector/test/lab01_vector_test.ml
+++ Lab01-vector/test/lab01_vector_test.ml
@@ -1,26 +1,8 @@
 open Alcotest
-
-module AlcotestExt = struct
-  let fixtures_parameterized ~(before : unit -> 'c) ~(after : 'c -> unit)
-      ~(params : 'a list) ~(param_to_string : 'a -> string)
-      ~(tests : (string * speed_level * ('c -> 'a -> unit)) list) =
-    tests
-    |> List.concat_map ~f:(fun (name, mode, tst) ->
-           List.map params ~f:(fun p ->
-               let name = name ^ " (" ^ param_to_string p ^ ")" in
-               let fn () =
-                 let c = before () in
-                 tst c p;
-                 after c
-               in
-               (name, mode, fn)))
-
-  let check_float ~msg ~expected ~actual ?(eps = 1e-6) =
-    check' ~msg ~expected:true ~actual:Float.(abs (expected - actual) <= eps)
-end
+open AlcotestExt
 
 let vf =
-  AlcotestExt.fixtures_parameterized
+  fixtures_parameterized
     ~before:(fun () ->
       let ret = ref 1 in
       Caml.Printf.printf "Before: %d\n" !ret;
