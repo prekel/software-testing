6e4a32abe9f66daa018f471ebdad7ecc5da06b73
Vladislav Prekel <misterptits@yandex.ru>
2021-12-01 12:06:43 +0700

    VEC doc

--- Lab01-vector/lib/vector.mli
+++ Lab01-vector/lib/vector.mli
@@ -1,5 +1,6 @@
 exception NotEnough of { need : int; got : int; failed_on : int }
 
+(** A vector with [n] dimensions which uses [float] as type for numbers *)
 module type VEC = sig
   (** [n] is dimensions of vector *)
   val n : int
@@ -13,25 +14,34 @@ module type VEC = sig
   (** [one] is vector with [1] in all dimensions *)
   val one : t
 
-  (** [of_list lst] creates the vector. Raises: [NotEnough] if [List.length lst < n] *)
+  (** [of_list lst] is vector created from list [lst]. Raises: [NotEnough] if [List.length lst < n] *)
   val of_list : float list -> t
 
+  (** [equal ~eps x y] is result of equality comparing with given accuracy [eps] of vectors [x] and [y] *)
   val equal : ?eps:float -> t -> t -> bool
 
+  (** [pp ~start ppf x] pretty-prints vector [x] using formatter [ppf] *)
   val pp : ?start:bool -> Formatter.t -> t -> unit
 
+  (** [show x] is string representation of vector [x] *)
   val show : t -> string
 
+  (** [add x y] is sum of vector [x] and vector [y] *)
   val add : t -> t -> t
 
+  (** [sub x y] is subtraction of vector [x] and vector [y] *)
   val sub : t -> t -> t
 
+  (** [mult x k] is vector [x] with lengths multiplied by [k] *)
   val mult : t -> float -> t
 
+  (** [dot_product x y] is scalar (dot) product of vector [x] and vector [y] *)
   val dot_product : t -> t -> float
 
+  (** [length_squared x] is length of vector [x] multiplied by self *)
   val length_squared : t -> float
 
+  (** [length x] is length of vector [x] *)
   val length : t -> float
 end
 
