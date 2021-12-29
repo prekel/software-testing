74328dd39d83236b868c0c7487e13fdd2eaa395c
Vladislav Prekel <misterptits@yandex.ru>
2021-12-29 15:35:15 +0700

    rasar

--- lab03-parser/lib/parser.ml
+++ lab03-parser/lib/parser.ml
@@ -1,8 +1,29 @@
 type token =
-  | Number
+  | Number of int
   | Plus
   | Minus
   | Mult
   | Div
   | EmptyString
   | Error
+
+let token_to_string = function
+  | Number n -> "Number" ^ Int.to_string n
+  | Plus -> "Plus"
+  | Minus -> "Minus"
+  | Mult -> "Mult"
+  | Div -> "Div"
+  | EmptyString -> "EmptyString"
+  | Error -> "Error"
+;;
+
+let process_line = function
+  | "+" -> Plus
+  | "-" -> Minus
+  | "*" -> Mult
+  | "/" -> Div
+  | "" -> EmptyString
+  | other ->
+    (try Number (Int.of_string other) with
+    | _ -> Error)
+;;
--- lab03-parser/test/parser_test.ml
+++ lab03-parser/test/parser_test.ml
@@ -1,3 +1,10 @@
 open Alcotest
+open Lab03_parser.Parser
 
-let suite = "Parser tests", []
+let test_empty_string () = 
+  let actual = process_line "" in 
+  let expected = EmptyString in
+  check' () ~msg:"" ~expected ~actual 
+
+
+let suite =  "", ["Empty string", `Quick, test_empty_string]
