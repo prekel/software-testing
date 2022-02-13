c4c507ab03d9bd2bb323788bc4cc650fa7d61bee
Vladislav Prekel <misterptits@yandex.ru>
2022-02-13 19:54:41 +0700

    mathing

--- lab03-parser/lib/calc.ml
+++ lab03-parser/lib/calc.ml
@@ -19,7 +19,7 @@ module States = struct
     | ErrorState of ('a, 'b) t
     | ErrorInput of ('a, 'b) t
     | ErrorOperation of ('a, 'b) t
-    | Finish
+    | Finish of 'a
 end
 
 module Actions = struct
@@ -28,25 +28,43 @@ module Actions = struct
     | Op of 'b
     | Empty
     | Invalid
+    | Calculate
+    | Back
+    | Reset
 end
 
 module StateMachine = struct
   open States
   open Actions
 
-  let update state action =
+  type a
+  type b
+
+  let doop (_acc : a) (_arg : a) (_op : b) : a option = None
+
+  let update (state : (a, b) States.t) action =
     match state, action with
+    | _, Reset -> WaitInitial
+    | _, Invalid -> ErrorInput state
     | WaitInitial, Num a -> WaitOperation { acc = a; acc' = a }
-    | WaitInitial, Op _ | WaitInitial, Empty -> ErrorState state
-    | WaitInitial, Invalid -> ErrorInput state
-    | WaitOperation _, Num _ -> ErrorState state
+    | WaitInitial, (Op _ | Empty | Calculate) -> ErrorState state
+    | WaitInitial, Back -> WaitInitial
+    | WaitOperation _, (Num _ | Calculate) -> ErrorState state
     | WaitOperation { acc; acc' }, Op op -> WaitArgument { acc; acc'; op }
-    | WaitOperation _, Empty -> Finish
-    | WaitOperation _, Invalid -> ErrorInput state
+    | WaitOperation { acc; _ }, Empty -> Finish acc
+    | WaitOperation _, Back -> WaitInitial
     | WaitArgument { acc; acc'; op }, Num arg -> Calculation { acc; acc'; op; arg }
-    | WaitArgument _, Op _ 
-    | WaitArgument _, Empty -> ErrorState state
-    | WaitArgument _, Invalid -> ErrorInput state
-    | _, _ -> state
+    | WaitArgument _, (Op _ | Empty | Calculate) -> ErrorState state
+    | WaitArgument { acc; acc'; _ }, Back -> WaitOperation { acc; acc' }
+    | Calculation _, (Empty | Num _ | Op _) -> ErrorState state
+    | Calculation { acc; acc'; op; _ }, Back -> WaitArgument { acc; acc'; op }
+    | Calculation { acc; acc'; op; arg }, Calculate ->
+      (match doop acc arg op with
+      | Some acc -> WaitOperation { acc; acc' }
+      | None -> ErrorOperation state)
+    | (ErrorState old_state | ErrorInput old_state | ErrorOperation old_state), Back ->
+      old_state
+    | (ErrorState _ | ErrorInput _ | ErrorOperation _), _ -> state
+    | Finish _, _ -> state
   ;;
 end
