ca1d8e9b597c546f7d8f8f33472bc2a689be8cf2
Vladislav Prekel <misterptits@yandex.ru>
2022-03-04 23:18:45 +0700

    button tests

--- app/test/test.ml
+++ app/test/test.ml
@@ -303,14 +303,12 @@ let%test_module "full component tests" =
 let%test_module "test with stub" =
   (module struct
     module StubCalculator : Calculator.S = struct
-      module I = Calc.MakeStateMachine (struct
+      include Calc.MakeStateMachine (struct
         type num = float [@@deriving sexp, equal]
         type op = [ `Sub ] [@@deriving sexp, equal]
 
         let calculate _ b c = Some (b -. c)
       end)
-
-      include I
 
       let line_to_action ?quit:_ = function
         | "number1" -> Num 123.
@@ -323,15 +321,17 @@ let%test_module "test with stub" =
     module Component = MakeComponent (StubCalculator)
     open Bonsai
 
-    let%expect_test "" =
-      let a = Bonsai.Var.create None in
+    let%expect_test "form test" =
+      (* Arrange *)
+      let state_var = Bonsai.Var.create None in
       let update_var =
-        Bonsai.Var.create (fun ac -> Effect.return @@ Bonsai.Var.set a (Some ac))
+        Bonsai.Var.create (fun ac -> Effect.return @@ Bonsai.Var.set state_var (Some ac))
       in
       let update = Bonsai.Var.value update_var in
       let handle = Handle.create (Result_spec.vdom Fn.id) (Component.form update) in
+      (* Act, Assert: initial state *)
       Handle.show handle;
-      print_s [%sexp (Bonsai.Var.get a : StubCalculator.action option)];
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
       [%expect
         {|
         <div>
@@ -349,15 +349,101 @@ let%test_module "test with stub" =
           </div>
         </div>
         () |}];
-      (* 2 *)
+      (* Act, Assert: inpun number1, but do not click *)
       Handle.input_text handle ~get_vdom:Fn.id ~selector:".form" ~text:"number1";
       Handle.show handle;
-      print_s [%sexp (Bonsai.Var.get a : StubCalculator.action option)];
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+        <div>
+          <form onsubmit>
+            <input class="form" #value="number1" oninput> </input>
+          </form>
+          <button class="enter" onclick> enter </button>
+          <div>
+            <button class="back" onclick> back </button>
+            <button class="calc" onclick> calc </button>
+            <button class="empty" onclick> empty </button>
+            <button class="invalid" onclick> invalid </button>
+            <button class="reset" onclick> reset </button>
+            <button class="quit" onclick> quit </button>
+          </div>
+        </div>
+        () |}];
+      (* Act, Assert: click on enter *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".enter";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+        <div>
+          <form onsubmit>
+            <input class="form" #value="" oninput> </input>
+          </form>
+          <button class="enter" onclick> enter </button>
+          <div>
+            <button class="back" onclick> back </button>
+            <button class="calc" onclick> calc </button>
+            <button class="empty" onclick> empty </button>
+            <button class="invalid" onclick> invalid </button>
+            <button class="reset" onclick> reset </button>
+            <button class="quit" onclick> quit </button>
+          </div>
+        </div>
+        ((Num 123)) |}];
+      (* Act, Assert: type end enter number2 *)
+      Handle.input_text handle ~get_vdom:Fn.id ~selector:".form" ~text:"number2";
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".enter";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+        <div>
+          <form onsubmit>
+            <input class="form" #value="" oninput> </input>
+          </form>
+          <button class="enter" onclick> enter </button>
+          <div>
+            <button class="back" onclick> back </button>
+            <button class="calc" onclick> calc </button>
+            <button class="empty" onclick> empty </button>
+            <button class="invalid" onclick> invalid </button>
+            <button class="reset" onclick> reset </button>
+            <button class="quit" onclick> quit </button>
+          </div>
+        </div>
+        ((Num -100)) |}];
+      (* Act, Assert: type end enter minus *)
+      Handle.input_text handle ~get_vdom:Fn.id ~selector:".form" ~text:"minus";
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".enter";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+        <div>
+          <form onsubmit>
+            <input class="form" #value="" oninput> </input>
+          </form>
+          <button class="enter" onclick> enter </button>
+          <div>
+            <button class="back" onclick> back </button>
+            <button class="calc" onclick> calc </button>
+            <button class="empty" onclick> empty </button>
+            <button class="invalid" onclick> invalid </button>
+            <button class="reset" onclick> reset </button>
+            <button class="quit" onclick> quit </button>
+          </div>
+        </div>
+        ((Op Sub)) |}];
+      (* Act, Assert: click on back *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".back";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
       [%expect
         {|
           <div>
             <form onsubmit>
-              <input class="form" #value="number1" oninput> </input>
+              <input class="form" #value="" oninput> </input>
             </form>
             <button class="enter" onclick> enter </button>
             <div>
@@ -369,28 +455,98 @@ let%test_module "test with stub" =
               <button class="quit" onclick> quit </button>
             </div>
           </div>
-          () |}];
-      (* 2 *)
-      Handle.click_on handle ~get_vdom:Fn.id ~selector:".enter";
+          (Back) |}];
+      (* Act, Assert: click on calc *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".calc";
       Handle.show handle;
-      print_s [%sexp (Bonsai.Var.get a : StubCalculator.action option)];
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
       [%expect
         {|
+          <div>
+            <form onsubmit>
+              <input class="form" #value="" oninput> </input>
+            </form>
+            <button class="enter" onclick> enter </button>
             <div>
-              <form onsubmit>
-                <input class="form" #value="" oninput> </input>
-              </form>
-              <button class="enter" onclick> enter </button>
-              <div>
-                <button class="back" onclick> back </button>
-                <button class="calc" onclick> calc </button>
-                <button class="empty" onclick> empty </button>
-                <button class="invalid" onclick> invalid </button>
-                <button class="reset" onclick> reset </button>
-                <button class="quit" onclick> quit </button>
-              </div>
+              <button class="back" onclick> back </button>
+              <button class="calc" onclick> calc </button>
+              <button class="empty" onclick> empty </button>
+              <button class="invalid" onclick> invalid </button>
+              <button class="reset" onclick> reset </button>
+              <button class="quit" onclick> quit </button>
             </div>
-            ((Num 123)) |}]
+          </div>
+          (Calculate) |}];
+      (* Act, Assert: click on empty *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".empty";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+          <div>
+            <form onsubmit>
+              <input class="form" #value="" oninput> </input>
+            </form>
+            <button class="enter" onclick> enter </button>
+            <div>
+              <button class="back" onclick> back </button>
+              <button class="calc" onclick> calc </button>
+              <button class="empty" onclick> empty </button>
+              <button class="invalid" onclick> invalid </button>
+              <button class="reset" onclick> reset </button>
+              <button class="quit" onclick> quit </button>
+            </div>
+          </div>
+          (Empty) |}];
+      (* Act, Assert: click on invalid *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".invalid";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+          <div>
+            <form onsubmit>
+              <input class="form" #value="" oninput> </input>
+            </form>
+            <button class="enter" onclick> enter </button>
+            <div>
+              <button class="back" onclick> back </button>
+              <button class="calc" onclick> calc </button>
+              <button class="empty" onclick> empty </button>
+              <button class="invalid" onclick> invalid </button>
+              <button class="reset" onclick> reset </button>
+              <button class="quit" onclick> quit </button>
+            </div>
+          </div>
+          ((Invalid "")) |}];
+      (* Act, Assert: click on reset *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".reset";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect
+        {|
+          <div>
+            <form onsubmit>
+              <input class="form" #value="" oninput> </input>
+            </form>
+            <button class="enter" onclick> enter </button>
+            <div>
+              <button class="back" onclick> back </button>
+              <button class="calc" onclick> calc </button>
+              <button class="empty" onclick> empty </button>
+              <button class="invalid" onclick> invalid </button>
+              <button class="reset" onclick> reset </button>
+              <button class="quit" onclick> quit </button>
+            </div>
+          </div>
+          (Reset) |}];
+      (* Act, Assert: click on quit (should fail because quit triggers reload) *)
+      Handle.click_on handle ~get_vdom:Fn.id ~selector:".quit";
+      Handle.show handle;
+      print_s [%sexp (Bonsai.Var.get state_var : StubCalculator.action option)];
+      [%expect.unreachable]
+      [@@expect.uncaught_exn
+        {| ("TypeError: Cannot read properties of undefined (reading 'reload')") |}]
     ;;
   end)
 ;;
